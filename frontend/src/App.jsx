7b4ffd32da6463ca1b8246936d1262107c8d4f8c
manavsharma111 <manavsharma3825@gmail.com>
2026-06-14 17:41:21 +0530

    Refactor URLs to use environment variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
 import CallOverlay from './components/CallOverlay'
-export const serverUrl = import.meta.env.VITE_BACKEND_URL || (window.location.hostname === 'localhost' ? "http://localhost:8000" : "https://social-media-app-mern-iota.vercel.app");
+export const serverUrl = import.meta.env.VITE_BACKEND_URL || (window.location.hostname === 'localhost' ? "http://localhost:8000" : "");
 function App() {
    getCurrentUser()
    getSuggestedUsers()
